8fe7fa56fa2b1d1e7ec0ca5f224b41a9fbbbb4f0
adastraz <tylergosox@gmail.com>
2020-02-14 12:56:30 -0500

    mvp, finished put and delete

--- data/projectRouter.js
+++ data/projectRouter.js
@@ -19,6 +19,10 @@ router.get('/:id', validateProjectID, (req, res) => {
     res.status(200).json(req.project)
 });
 
+router.get('/:id/actions', validateProjectActionsID, (req, res) => {
+    res.status(200).json(req.project)
+});
+
 router.post('/', validateProject, (req, res) => {
     console.log(`router post ${req.body}`)
     Projects.insert(req.body)
@@ -28,6 +32,28 @@ router.post('/', validateProject, (req, res) => {
         .catch(err => {
             console.log(err)
             res.status(500).json({ message: 'There was an error while saving this project to the database' })
+        })
+});
+
+router.delete('/:id', validateProjectID, (req, res) => {
+    Projects.remove(req.params.id)
+        .then(removed => {
+            res.status(200).json(removed)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: 'The project could not be removed'})
+        })
+});
+
+router.put('/:id', validateProjectID, validateProject, (req, res) => {
+    Projects.update(req.params.id, req.body)
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: 'Project cannot be updated' })
         })
 });
 
@@ -42,6 +68,18 @@ function validateProject(req, res, next) {
 
 function validateProjectID (req, res, next) {
     Projects.get(req.params.id)
+        .then(project => {
+            project ? (req.project = project) & next() : 
+            res.status(400).json({ message: "Error retrieving the project" })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: "Error retrieving the project" })
+        })
+}
+
+function validateProjectActionsID (req, res, next) {
+    Projects.getProjectActions(req.params.id)
         .then(project => {
             project ? (req.project = project) & next() : 
             res.status(400).json({ message: "Error retrieving the project" })
